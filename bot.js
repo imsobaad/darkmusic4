92a94c5faf1be2c938b60fad6d4ce901cc5b3cd5
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-11-16 15:09:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -265,7 +265,9 @@ client.on("message", message => {
    });
 
 
-
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    client.channels.get("509813780318781452").join(); //by : ....
+});
 
 
 client.login(process.env.BOT_TOKEN);
