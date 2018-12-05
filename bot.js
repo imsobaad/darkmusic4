f1d8f449061b0a8ffa906fde5f0e9db189692f9b
imsobaad <41808334+imsobaad@users.noreply.github.com>
2018-12-05 22:50:42 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,11 @@ const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '4';
 
 
+client.on('ready', () => { //code bot not leave room voice //Bot Is Online
+    client.channels.get("519992756316405780").join(); //by : ....
+});
+
+
 
 
 client.on('message', function(message) {
